94e5b21398cb7ee73cf2f77af0c0c5159e709a13
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2025-01-30 19:10:48 +0530

    book get by book id method complete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,4 @@
-
+const books = require("../models/booksModel")
 
 // List all books with pagination
 exports.getAllBooks = (req, res) => {
@@ -9,6 +9,14 @@ exports.getAllBooks = (req, res) => {
 // Get a book by ID
 exports.getBookById = (req, res) => {
 
+    const bookId = parseInt(req.params.id);
+    const book = books.find(b => b.id == bookId);
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(book);
 };
 
 
